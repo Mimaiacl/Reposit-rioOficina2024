5e02b1ec3b95f5d0d6082cb310abda1c1d074141
Mimaiacl <160976279+Mimaiacl@users.noreply.github.com>
2024-05-14 19:46:50 -0300

    .

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-let nomeUsuário = "";
-let elemento = document.querySelector("#nome-usuário");
+alert{("olá mundo"); 
 
-while (nomeUsuário == ""){
-    nomeUsuário = prompt("Qual o seu nome?");
 }
-    if (nomeUsuário == null){
-        elemento.textContent = 'seja muito bem vindo.';
-}else{
-    elemento.textContent = nomeUsuário;
-}
-
-
-
 
